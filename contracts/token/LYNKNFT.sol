a7cf1cdfef0e3c8467e05b1efc944855835e655c
alyxgood <alyx2821@gmail.com>
2022-10-23 17:13:27 +0800

    feat: upgradeWithPermit

--- contracts/token/LYNKNFT.sol
+++ contracts/token/LYNKNFT.sol
@@ -46,47 +46,13 @@ contract LYNKNFT is ILYNKNFT, ERC721EnumerableUpgradeable, baseContract {
         _mint(_tokenId, _payment);
     }
 
-    function upgrade(Attribute _attr, uint256 _tokenId, uint256 _point, address _payment) external payable {
-        require(
-            IUser(DBContract(DB_CONTRACT).USER_INFO()).isValidUser(_msgSender()),
-                'LYNKNFT: not a valid user.'
-        );
+    function upgradeWithPermit(Attribute _attr, uint256 _tokenId, uint256 _point, address _payment, uint256 _amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
+        IERC20PermitUpgradeable(_payment).permit(_msgSender(), address(this), _amount, deadline, v, r, s);
+        _upgrade(_attr, _tokenId, _point, _payment);
+    }
 
-        // avoid upgrade while staking
-        require(
-            tx.origin == _msgSender() &&
-            ERC721Upgradeable.ownerOf(_tokenId) == _msgSender(),
-                'LYNKNFT: not the owner'
-        );
-
-        if (Attribute.charisma == _attr) {
-            require(
-                _payment == DBContract(DB_CONTRACT).USDT_TOKEN() ||
-                _payment == DBContract(DB_CONTRACT).LYNK_TOKEN(),
-                    'LYNKNFT: unsupported payment.'
-            );
-        } else {
-            uint256 preAttrIndex = uint256(_attr) - 1;
-            (uint256 preAttrLevel,) = DBContract(DB_CONTRACT).calcLevel(Attribute(preAttrIndex), nftInfo[_tokenId][preAttrIndex]);
-            (uint256 curAttrLevelAfterUpgrade, uint256 curAttrLevelOverflowAfterUpgrade) = DBContract(DB_CONTRACT).calcLevel(_attr, _point + nftInfo[_tokenId][uint256(_attr)]);
-            require(
-                preAttrLevel > curAttrLevelAfterUpgrade ||
-                (preAttrLevel == curAttrLevelAfterUpgrade && curAttrLevelOverflowAfterUpgrade == 0),
-                    'LYNKNFT: level overflow.'
-            );
-
-            require(_payment == DBContract(DB_CONTRACT).AP_TOKEN(), 'LYNKNFT: unsupported payment.');
-        }
-
-        uint256 decimal = IERC20MetadataUpgradeable(_payment).decimals();
-        uint256 amount = _point * (10 ** decimal);
-        _pay(_payment, _msgSender(), amount);
-
-        nftInfo[_tokenId][uint256(_attr)] += _point;
-        emit Upgrade(_tokenId, _attr, _point);
-
-        // dealing with the ref things.
-        IUser(DBContract(DB_CONTRACT).USER_INFO()).hookByUpgrade(_msgSender(), Attribute.charisma == _attr ? _point : 0);
+    function upgrade(Attribute _attr, uint256 _tokenId, uint256 _point, address _payment) external {
+        _upgrade(_attr, _tokenId, _point, _payment);
     }
 
     function nftInfoOf(uint256 _tokenId) external view override returns (uint256[] memory _nftInfo) {
@@ -157,5 +123,48 @@ contract LYNKNFT is ILYNKNFT, ERC721EnumerableUpgradeable, baseContract {
         (uint256 vitality, uint256 intellect) = _attributesGen(_msgSender());
         nftInfo[_tokenId] = [ vitality, intellect, 0, 0];
         ERC721Upgradeable._safeMint(_msgSender(), _tokenId);
+    }
+
+    function _upgrade(Attribute _attr, uint256 _tokenId, uint256 _point, address _payment) private {
+        require(
+            IUser(DBContract(DB_CONTRACT).USER_INFO()).isValidUser(_msgSender()),
+            'LYNKNFT: not a valid user.'
+        );
+
+        // avoid upgrade while staking
+        require(
+            tx.origin == _msgSender() &&
+            ERC721Upgradeable.ownerOf(_tokenId) == _msgSender(),
+            'LYNKNFT: not the owner'
+        );
+
+        if (Attribute.charisma == _attr) {
+            require(
+                _payment == DBContract(DB_CONTRACT).USDT_TOKEN() ||
+                _payment == DBContract(DB_CONTRACT).LYNK_TOKEN(),
+                'LYNKNFT: unsupported payment.'
+            );
+        } else {
+            uint256 preAttrIndex = uint256(_attr) - 1;
+            (uint256 preAttrLevel,) = DBContract(DB_CONTRACT).calcLevel(Attribute(preAttrIndex), nftInfo[_tokenId][preAttrIndex]);
+            (uint256 curAttrLevelAfterUpgrade, uint256 curAttrLevelOverflowAfterUpgrade) = DBContract(DB_CONTRACT).calcLevel(_attr, _point + nftInfo[_tokenId][uint256(_attr)]);
+            require(
+                preAttrLevel > curAttrLevelAfterUpgrade ||
+                (preAttrLevel == curAttrLevelAfterUpgrade && curAttrLevelOverflowAfterUpgrade == 0),
+                'LYNKNFT: level overflow.'
+            );
+
+            require(_payment == DBContract(DB_CONTRACT).AP_TOKEN(), 'LYNKNFT: unsupported payment.');
+        }
+
+        uint256 decimal = IERC20MetadataUpgradeable(_payment).decimals();
+        uint256 amount = _point * (10 ** decimal);
+        _pay(_payment, _msgSender(), amount);
+
+        nftInfo[_tokenId][uint256(_attr)] += _point;
+        emit Upgrade(_tokenId, _attr, _point);
+
+        // dealing with the ref things.
+        IUser(DBContract(DB_CONTRACT).USER_INFO()).hookByUpgrade(_msgSender(), Attribute.charisma == _attr ? _point : 0);
     }
 }
