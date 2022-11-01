e42afa4628e667fb57547dd859e8f97aceadabda
alyxgood <alyx2821@gmail.com>
2022-11-01 19:04:47 +0800

    feat: add deployments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -73,6 +73,17 @@ const config: HardhatUserConfig = {
         initialIndex: 0,
         count: 14
       }
+    },
+    alyx: {
+      url: process.env.ALYX_RPC,
+      chainId: 1314,
+      gasMultiplier: 1.5,
+      accounts: {
+        mnemonic: process.env.MNEMONIC_ALYX ? process.env.MNEMONIC_ALYX : "test test test test test test test test test test test junk",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 14
+      }
     }
   },
   gasReporter: {
